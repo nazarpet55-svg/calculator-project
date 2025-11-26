16b532c2340ea3a508ffce30beeba9ff9ee459e4
Dworf & beer inc <nazarpet55@gmail.com>
2025-11-26 12:44:22 +0200

    Added modulo operation to calculator

--- calculator.py
+++ calculator.py
@@ -21,9 +21,16 @@ def power(a, b):
     """Піднесення числа a до степеня b"""
     return a ** b
 
+def modulo(a, b):
+   """Остача від ділення"""
+   if b != 0:
+    return a % b
+   else:
+    return "Помилка: ділення на нуль!"
+
 
 print("=== Простий калькулятор ===")
-print("Операції: +, -, *, /, **")
+print("Операції: +, -, *, /, **, %")
 print("Для виходу введіть 'exit'")
 
 while True:
@@ -33,7 +40,7 @@ while True:
         print("До побачення!")
         break
 
-    if operation not in ['+', '-', '*', '/', '**']:
+    if operation not in [‘+’, ‘-’, ‘*’, ‘/’, ‘**’, ‘%’]:
         print("Невірна операція!")
         continue
 
@@ -51,6 +58,8 @@ while True:
             result = divide(num1, num2)
         elif operation == '**':
             result = power(num1, num2)
+        elif operation == '%':
+            result = modulo(num1, num2)
 
         print(f"Результат: {result}")
 
